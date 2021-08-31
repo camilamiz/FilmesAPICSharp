e434fe487280f192d7e0599da75b0ab9b6caf369
camilamiz <camila.yuri.m@gmail.com>
2021-08-31 17:29:09 -0300

    First post request and model validations

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -1,11 +1,24 @@
-﻿using System;
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace FilmesAPI.Controllers
 {
-    public class FilmeController
+    [ApiController]
+    [Route("[controller]")]
+
+    public class FilmeController : ControllerBase
     {
+        private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        public void AdicionaFilme([FromBody] Filme filme)
+        {
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+        }
     }
 }
