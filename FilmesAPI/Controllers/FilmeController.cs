61ae1fd471852f23bfe72cec2755a8021ead6baf
camilamiz <camila.yuri.m@gmail.com>
2021-08-31 17:46:03 -0300

    Retrieve request.

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -13,12 +13,26 @@ namespace FilmesAPI.Controllers
     public class FilmeController : ControllerBase
     {
         private static List<Filme> filmes = new List<Filme>();
+        private static int id = 1;
 
         [HttpPost]
         public void AdicionaFilme([FromBody] Filme filme)
         {
             filmes.Add(filme);
+            filme.Id = id++;
             Console.WriteLine(filme.Titulo);
+        }
+
+        [HttpGet]
+        public IEnumerable<Filme> RecuperaFilmes()
+        {
+            return filmes;
+        }
+
+        [HttpGet("{id}")]
+        public Filme RecuperaFilmePorId(int id)
+        {
+            return filmes.FirstOrDefault(filme => filme.Id == id);
         }
     }
 }
--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -16,5 +16,6 @@ namespace FilmesAPI.Models
         public string Genero { get; set; }
         [Range(1, 600, ErrorMessage ="A duração deve ter no mínimo 1 e no máximo 600 minutos")]
         public int Duracao { get; set; }
+        public int Id { get; internal set; }
     }
 }
